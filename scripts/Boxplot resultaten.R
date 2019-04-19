8537e9b6e41430c94f73a2ad7b88c9e329d92623
Robbie verdurme <boeferrob@live.be>
2019-04-19 17:42:45 +0200

    Paper boxplot img toegevoegd

--- scripts/Boxplot resultaten.R
+++ scripts/Boxplot resultaten.R
@@ -1,5 +1,6 @@
+#Data inlezen
 mijnData <- read.csv(file.choose())
-boxplot(score_1 ~ rt/muziek ,data = mijnData, main="Resultatenanalyse",ylab="Variabelen", xlab="Score",horizontal=TRUE, col = "skyblue")
+data <- mijnData
 
 #Waarden aanpassen voor titels boxplot
 mijnData$rt <- as.character(mijnData$rt)
@@ -10,3 +11,39 @@ mijnData$rt[mijnData$rt=="Nee"] <- "Zonder Retrieval Practice"
 
 mijnData$muziek[mijnData$muziek=="Ja"] <- "Met Muziek"
 mijnData$muziek[mijnData$muziek=="Nee"] <- "Zonder Muziek"
+
+#Data vergelijken enkel retrieval practice
+boxplot(score_1 ~ rt ,data = data, main="Resultatenanalyse Score 1",ylab="Retrieval Practice", xlab="Score",horizontal=TRUE, col = "skyblue")
+boxplot(score_2 ~ rt ,data = data, main="Resultatenanalyse Score 2",ylab="Retrieval Practice", xlab="Score",horizontal=TRUE, col = "skyblue")
+
+#Data vergelijken enkel muziek
+boxplot(score_1 ~ muziek ,data = data, main="Resultatenanalyse Score 1",ylab="Muziek", xlab="Score",horizontal=TRUE, col = "skyblue")
+boxplot(score_2 ~ muziek ,data = data, main="Resultatenanalyse Score 2",ylab="Muziek", xlab="Score",horizontal=TRUE, col = "skyblue")
+
+#Data vergelijken met retrieval practice en muziek
+boxplot(score_1 ~ rt/muziek ,data = mijnData, main="Resultatenanalyse Score 1",ylab="Variabelen", xlab="Score",horizontal=TRUE, col = "skyblue")
+boxplot(score_2 ~ rt/muziek ,data = mijnData, main="Resultatenanalyse Score 2",ylab="Variabelen", xlab="Score",horizontal=TRUE, col = "skyblue")
+
+#Data vergelijken met Muziek
+MetMuziek_score1 <- c(data$score_1[data$muziek=="Ja"]) #count 86
+MetMuziek_score2 <- c(data$score_2[data$muziek=="Ja"]) #count 89
+test <- c("Test 1", "Test 2")
+
+boxplot(MetMuziek_score1, MetMuziek_score2, names = test, main="Resultatenanalyse met muziek", ylab="Testen", xlab="Totaal Score", horizontal = TRUE,col = "skyblue")
+
+#Data vergelijken zonder muziek
+ZonderMuziek_score1 <- c(data$score_1[data$muziek=="Nee"]) #count 101
+ZonderMuziek_score2 <- c(data$score_2[data$muziek=="Nee"]) #count 81
+boxplot(ZonderMuziek_score1, ZonderMuziek_score2, names = scores, main="Resultatenanalyse Zonder muziek", ylab="Testen", xlab="Totaal Score", horizontal = TRUE,col = "skyblue")
+
+#Data vergelijken met rt
+MetRT_score1 <- c(data$score_1[data$rt=="Ja "]) #count 99
+MetRT_score2 <- c(data$score_2[data$rt=="Ja "]) #count 90
+
+boxplot(MetRT_score1, MetRT_score2, names = test, main="Resultatenanalyse met Retrieval Practice", ylab="Testen", xlab="Totaal Score", horizontal = TRUE,col = "skyblue")
+
+#Data vergelijken zonder rt
+ZonderRT_score1 <- c(data$score_1[data$rt=="Nee"]) #count 88
+ZonderRT_score2 <- c(data$score_2[data$rt=="Nee"]) #count 80
+
+boxplot(ZonderRT_score1, ZonderRT_score2, names = test, main="Resultatenanalyse zonder Retrieval Practice", ylab="Testen", xlab="Totaal Score", horizontal = TRUE,col = "skyblue")
---
+++ scripts/count_not_NA.R
@@ -0,0 +1,2 @@
+length(ZonderMuziek_score1) - (sum(length(which(is.na(ZonderMuziek_score1)))))
+
